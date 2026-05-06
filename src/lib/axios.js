6561259ad647987701905cf61550bb846b1a79e1
andreasmrcl <andreasmrcl2@gmail.com>
2026-05-06 20:25:43 +0700

    fix axios

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,7 @@
 import Axios from "axios";
 
 const axios = Axios.create({
-  baseURL: "", // KOSONGKAN BAGIAN INI SANGAT PENTING!
+  baseURL: 'https://api.konsilveteriner.com',
   withCredentials: true,
   headers: {
     "Accept": "application/json",
